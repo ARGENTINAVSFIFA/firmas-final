ba2b2a472894dfd8fc015406d9f4419182e27d21
ARGENTINAVSFIFA <elreyaxel15@gmail.com>
2026-07-20 21:19:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-let firmas = 0;
+let firmas = localStorage.getItem("firmas") || 0;
 const meta = 500000;
 
+document.getElementById("contador").innerHTML =
+    Number(firmas).toLocaleString("es-ES") +
+    " / " +
+    meta.toLocaleString("es-ES") +
+    " firmas";
+
+document.getElementById("progreso").style.width =
+    (firmas / meta) * 100 + "%";
+
 function firmar() {
+
+    if (localStorage.getItem("yaFirmo")) {
+        alert("❌ Ya has firmado esta petición.");
+        return;
+    }
 
     const nombre = prompt("Escribe tu nombre:");
 
@@ -9,15 +23,17 @@ function firmar() {
 
     firmas++;
 
+    localStorage.setItem("firmas", firmas);
+    localStorage.setItem("yaFirmo", "si");
+
     document.getElementById("contador").innerHTML =
-        firmas.toLocaleString("es-ES") +
+        Number(firmas).toLocaleString("es-ES") +
         " / " +
         meta.toLocaleString("es-ES") +
         " firmas";
 
-    const porcentaje = (firmas / meta) * 100;
-
-    document.getElementById("progreso").style.width = porcentaje + "%";
+    document.getElementById("progreso").style.width =
+        (firmas / meta) * 100 + "%";
 
     alert("✅ Gracias por firmar, " + nombre + "!");
 }
