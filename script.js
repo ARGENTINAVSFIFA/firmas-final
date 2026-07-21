33121c4c6affb2d34de163afd8ed12b1fd3adfc8
ARGENTINAVSFIFA <elreyaxel15@gmail.com>
2026-07-21 01:54:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,5 @@
-let firmas = localStorage.getItem("firmas") || 0;
-const meta = 500000;
-
-document.getElementById("contador").innerHTML =
-    Number(firmas).toLocaleString("es-ES") +
-    " / " +
-    meta.toLocaleString("es-ES") +
-    " firmas";
-
-document.getElementById("progreso").style.width =
-    (firmas / meta) * 100 + "%";
+alert("script.js se cargó correctamente");
 
 function firmar() {
-
-    if (localStorage.getItem("yaFirmo")) {
-        alert("❌ Ya has firmado esta petición.");
-        return;
-    }
-
-    const nombre = prompt("Escribe tu nombre:");
-
-    if (!nombre) return;
-
-    firmas++;
-
-    localStorage.setItem("firmas", firmas);
-    localStorage.setItem("yaFirmo", "si");
-
-    document.getElementById("contador").innerHTML =
-        Number(firmas).toLocaleString("es-ES") +
-        " / " +
-        meta.toLocaleString("es-ES") +
-        " firmas";
-
-    document.getElementById("progreso").style.width =
-        (firmas / meta) * 100 + "%";
-
-    alert("✅ Gracias por firmar, " + nombre + "!");
+    alert("¡El botón funciona!");
 }
